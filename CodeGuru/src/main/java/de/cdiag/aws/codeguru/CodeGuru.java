8c21219b2afbdb63b8d2601f5d5e47911d2ab498
Stefan Meier (CDI AG) <stefan.meier@cdi-ag.de>
2020-01-20 10:44:15 +0100

    HealthCheck implemented

--- CodeGuru/src/main/java/de/cdiag/aws/codeguru/CodeGuru.java
+++ CodeGuru/src/main/java/de/cdiag/aws/codeguru/CodeGuru.java
@@ -1,6 +1,5 @@
 package de.cdiag.aws.codeguru;
 
-import org.apache.log4j.BasicConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.Banner;
@@ -15,12 +14,10 @@ public class CodeGuru {
     public static final Logger LOGGER = LoggerFactory.getLogger(CodeGuru.class);
 
     public static void main(String[] args) {
-        BasicConfigurator.configure();
-        System.err.println("test");
-        LOGGER.info("CDI AG Profiler started");
-        new Profiler.Builder()
-                .profilingGroupName("CDI_AG")
-                .build().start();
+//        LOGGER.info("CDI AG Profiler started");
+//        new Profiler.Builder()
+//                .profilingGroupName("CDI_AG")
+//                .build().start();
 
         new SpringApplicationBuilder(CodeGuru.class)
                 .bannerMode(Banner.Mode.OFF)

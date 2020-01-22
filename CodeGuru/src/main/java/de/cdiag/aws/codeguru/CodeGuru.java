88f730ec7ee833c70045f8085cb1b7c6675a23d7
Stefan Meier (CDI AG) <stefan.meier@cdi-ag.de>
2020-01-22 10:35:18 +0100

    test

--- CodeGuru/src/main/java/de/cdiag/aws/codeguru/CodeGuru.java
+++ CodeGuru/src/main/java/de/cdiag/aws/codeguru/CodeGuru.java
@@ -48,6 +48,16 @@ public class CodeGuru implements CommandLineRunner {
 
 
     public static void main(String[] args) {
+
+        int len = Integer.parseInt(args[0]);
+        char[] thing = new char[len];
+
+        for (int i=0; i<thing.length; i++) {
+            thing[i] = 'x';
+        }
+
+        System.out.println("Did a thing");
+
         new SpringApplicationBuilder(CodeGuru.class)
                 .bannerMode(Banner.Mode.OFF)
                 .run(args);
